1761485619cc1ee2c8b08af5db4af96e0ef2cd61
sudan <ssudan16@gmail.com>
2016-04-08 22:25:39 +0530

    Adding generator for id

---
+++ src/main/scala/com/promotion/rule_engine/generator/IdGenerator.scala
@@ -0,0 +1,13 @@
+package com.promotion.rule_engine.generator
+
+import scala.util.Random
+
+object IdGenerator {
+
+    def generate(prefix: String, totLen: Int): String = {
+        val timestampStr = (System.currentTimeMillis / 60000).toString
+        val randomPartLen = totLen - prefix.length - timestampStr.length
+        val randomStr = Random.alphanumeric.take(randomPartLen).mkString.toUpperCase
+        prefix + timestampStr + randomStr
+    }
+}
