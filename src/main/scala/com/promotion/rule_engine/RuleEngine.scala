56311650025b3618ac10f893ed13bcd9bf3ce727
sudan <ssudan16@gmail.com>
2016-04-08 19:30:12 +0530

    bootstrap redis database

--- src/main/scala/com/promotion/rule_engine/RuleEngine.scala
+++ src/main/scala/com/promotion/rule_engine/RuleEngine.scala
@@ -2,6 +2,7 @@ package com.promotion.rule_engine
 
 import com.promotion.rule_engine.bootstrap.ConfigManager
 import com.promotion.rule_engine.bootstrap.MongoClient
+import com.promotion.rule_engine.bootstrap.RedisClient
 
 object RuleEngine extends App {
 
@@ -9,5 +10,10 @@ object RuleEngine extends App {
 
 	val mongoURL = ConfigManager.getValue("mongo.uri")
 	val mongoDBName = ConfigManager.getValue("mongo.dbName")
-	val db = MongoClient.init(mongoURL, mongoDBName)
+	val mongoConn = MongoClient.init(mongoURL, mongoDBName)
+
+	val redisHostName = ConfigManager.getValue("redis.hostName")
+	val redisPort = (ConfigManager.getValue("redis.portNumber")).toInt
+	val redisConn = RedisClient.init(redisHostName, redisPort)
+
 }
---
+++ src/main/scala/com/promotion/rule_engine/bootstrap/RedisClient.scala
@@ -0,0 +1,11 @@
+package com.promotion.rule_engine.bootstrap
+
+import com.redis.{RedisClient => RedisDBClient}
+
+object RedisClient {
+
+	def init(hostName: String, port: Int) = {
+		val db = new RedisDBClient(hostName, port)
+		db
+	}
+}
