b40093ab2f91f40dad9bfb8e168d3c767b08d9e2
sudan <ssudan16@gmail.com>
2016-04-08 21:13:00 +0530

    try new formatting

--- src/main/scala/com/promotion/rule_engine/model/Rule.scala
+++ src/main/scala/com/promotion/rule_engine/model/Rule.scala
@@ -2,16 +2,19 @@ package com.promotion.rule_engine.model
 
 import java.util.Date
 
-case class Rule(id: String,
-				name: String,
-				description: String,
-				createdOn: Date,
-				modifiedAt: Date,
-				version: String,
-				owner: String,
-				discount: Double,
-				boost: Integer,
-				properties: Map[String, List[String]],
-				regionList: RegionList,
-				categoryList: CategoryList,
-				isActive: Boolean)
+case class Rule(
+		id: String,
+		name: String,
+		description: String,
+		createdOn: Date,
+		modifiedAt: Date,
+		version: String,
+		owner: String,
+		discount: Double,
+		boost: Integer,
+		properties: Map[String, List[String]],
+		regionList: RegionList,
+		categoryList: CategoryList,
+		isActive: Boolean
+	)
+
