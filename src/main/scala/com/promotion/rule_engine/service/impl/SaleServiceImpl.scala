799edd26f9deb3eaf30b7279e97f7af38826f815
sudan <ssudan16@gmail.com>
2016-04-30 22:34:15 +0530

    code refactor

--- src/main/scala/com/promotion/rule_engine/service/impl/SaleServiceImpl.scala
+++ src/main/scala/com/promotion/rule_engine/service/impl/SaleServiceImpl.scala
@@ -2,7 +2,7 @@ package com.promotion.rule_engine.service.impl
 
 import com.promotion.rule_engine.converter.SaleConverter
 import com.promotion.rule_engine.dao.impl.{RuleDaoImpl, CampaignDaoImpl, SaleDaoImpl}
-import com.promotion.rule_engine.model.{RuleRelationship, Campaign, Rule}
+import com.promotion.rule_engine.model.Rule
 import com.promotion.rule_engine.service.api.SaleService
 import play.api.libs.json.JsValue
 
